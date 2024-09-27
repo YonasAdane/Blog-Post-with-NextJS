16c582a321eb850876efb2d11fa259cdb0d4533d
YonasAdane <yonasadane94@gmail.com>
2024-09-27 14:10:11 +0300

    footer added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-zinc-900 bg-zinc-100`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-zinc-900 bg-zinc-100 flex flex-col min-h-screen`}
       >
         <Header/>
         {children}
